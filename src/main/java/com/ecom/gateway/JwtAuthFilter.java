97044ba2e97a2359ea3add25abbe772f817c0124
Sayan Shil <sayansil2000@gmail.com>
2026-02-26 00:11:48 +0530

    Microservices: API Gateway configured

---
+++ src/main/java/com/ecom/gateway/JwtAuthFilter.java
@@ -0,0 +1,23 @@
+package com.ecom.gateway;
+
+import org.apache.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String s = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if(s==null || !s.startsWith("Bearer ")){
+            exchange.getResponse().setStatusCode(HttpStatusCode.valueOf(HttpStatus.SC_UNAUTHORIZED));
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+
+    }
+}
